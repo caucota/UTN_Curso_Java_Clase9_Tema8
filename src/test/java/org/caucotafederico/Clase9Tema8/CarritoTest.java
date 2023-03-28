6ef0100908d579221a5bffde28487003ebbb9e54
caucota <50638087+caucota@users.noreply.github.com>
2023-03-28 08:29:55 -0300

    casos test

--- src/test/java/org/caucotafederico/Clase9Tema8/CarritoTest.java
+++ src/test/java/org/caucotafederico/Clase9Tema8/CarritoTest.java
@@ -1,6 +1,8 @@
 package org.caucotafederico.Clase9Tema8;
 
 import static org.junit.Assert.assertEquals;
+
+import org.caucotafederico.Clase9Tema8.exceptions.DescuentoCarritoCeroException;
 import org.junit.Test;
 
 import Entities.Carrito;
@@ -13,9 +15,9 @@ import Entities.Persona;
 import Entities.Producto;
 
 public class CarritoTest {
-/*
+	/*
 	@Test
-	public void test1_probarTotalCarrito() {
+	public void test1_probarTotalCarrito()  {
 		Producto prodTest = new Producto("", 0.0);
 	
 		Descuento descuentoCarrito = new DescuentoPorMonto("SIN DESCUENTO", 0.0);		
@@ -24,62 +26,76 @@ public class CarritoTest {
 		carritoCompra.setDesc(descuentoCarrito);
 		
 		prodTest = new Producto("Pan", 25.0 );
-		carritoCompra.appendItemCarito(new ItemCarrito(prodTest, 1));
+		carritoCompra.appendItemCarrito(new ItemCarrito(prodTest, 1));
 		prodTest = new Producto("Queso", 100.0 );
-		carritoCompra.appendItemCarito(new ItemCarrito(prodTest, 2));
+		carritoCompra.appendItemCarrito(new ItemCarrito(prodTest, 2));
 		prodTest = new Producto("Leche", 85.5 );
-		carritoCompra.appendItemCarito(new ItemCarrito(prodTest, 6));
+		carritoCompra.appendItemCarrito(new ItemCarrito(prodTest, 6));
 
 		Double rtdoEsperado = 738.0;
 
 		
-		assertEquals(rtdoEsperado, carritoCompra.calcularTotalConDescuento());
+		try {
+			assertEquals(rtdoEsperado, carritoCompra.calcularTotalConDescuento());
+		} catch (DescuentoCarritoCeroException e) {
+			System.out.println(e.getMensaje());
+		}
 	
 	}
 	
+	*/
 	
 	@Test
 	public void test2_probarTotalCarrito() {
 		Producto prodTest = new Producto("", 0.0);
 	
-		Descuento descuentoCarrito = new DescuentoPorMonto("SIN DESCUENTO", 0.0);		
+		Descuento descuentoCarrito = new DescuentoPorMonto("DESCUENTO FIJO", 10.0);		
 		Persona cliente = new Persona("Perez", "Juan", "1512455");
 		Carrito carritoCompra = new Carrito(cliente);
 		carritoCompra.setDesc(descuentoCarrito);
 		
-		prodTest = new Producto("Pan", 25.0 );
-		carritoCompra.appendItemCarito(new ItemCarrito(prodTest, 1));
-		prodTest = new Producto("Queso", 100.0 );
-		carritoCompra.appendItemCarito(new ItemCarrito(prodTest, 2));
-		prodTest = new Producto("Leche", 85.5 );
-		carritoCompra.appendItemCarito(new ItemCarrito(prodTest, 6));
+		prodTest = new Producto("Pan", 0.0 );
+		carritoCompra.appendItemCarrito(new ItemCarrito(prodTest, 1));
+		prodTest = new Producto("Queso", 0.0 );
+		carritoCompra.appendItemCarrito(new ItemCarrito(prodTest, 2));
+		prodTest = new Producto("Leche", 0.0 );
+		carritoCompra.appendItemCarrito(new ItemCarrito(prodTest, 6));
 
-		Double rtdoEsperado = 738.0;
+		Double rtdoEsperado = 0.0;
 		
-		assertEquals(rtdoEsperado, carritoCompra.calcularTotalConDescuento());
+		try {
+			assertEquals(rtdoEsperado, carritoCompra.calcularTotalConDescuento());
+		} catch (DescuentoCarritoCeroException e) {
+			System.out.println(e.getMensaje());
+		}
 	}
-	
+	 
 	@Test
 	public void test3_probarConDescuentoFijo() {
 		Producto prodTest = new Producto("", 0.0);
 	
-		Descuento descuentoCarrito = new DescuentoPorMonto("DESCUENTO FIJO", 80.0);		
+		Descuento descuentoCarrito = new DescuentoPorMonto("DESCUENTO FIJO", 1880.0);		
 		Persona cliente = new Persona("Perez", "Juan", "1512455");
 		Carrito carritoCompra = new Carrito(cliente);
 		carritoCompra.setDesc(descuentoCarrito);
 		
 		prodTest = new Producto("Pan", 25.0 );
-		carritoCompra.appendItemCarito(new ItemCarrito(prodTest, 1));
+		carritoCompra.appendItemCarrito(new ItemCarrito(prodTest, 1));
 		prodTest = new Producto("Queso", 100.0 );
-		carritoCompra.appendItemCarito(new ItemCarrito(prodTest, 2));
+		carritoCompra.appendItemCarrito(new ItemCarrito(prodTest, 2));
 		prodTest = new Producto("Leche", 85.5 );
-		carritoCompra.appendItemCarito(new ItemCarrito(prodTest, 6));
+		carritoCompra.appendItemCarrito(new ItemCarrito(prodTest, 6));
 
 		Double rtdoEsperado = 1140.0;
 		
-		assertEquals(rtdoEsperado, carritoCompra.calcularTotalConDescuento());
+		try {
+			assertEquals(rtdoEsperado, carritoCompra.calcularTotalConDescuento());
+		} catch (DescuentoCarritoCeroException e) {
+			System.out.println(e.getMensaje());
+		}
 	}
 	
+	/*
 	@Test
 	public void test4_probarConDescuentoPorPorcentaje() {
 		Producto prodTest = new Producto("", 0.0);
@@ -90,21 +106,25 @@ public class CarritoTest {
 		carritoCompra.setDesc(descuentoCarrito);
 		
 		prodTest = new Producto("Pan", 25.0 );
-		carritoCompra.appendItemCarito(new ItemCarrito(prodTest, 1));
+		carritoCompra.appendItemCarrito(new ItemCarrito(prodTest, 1));
 		prodTest = new Producto("Queso", 100.0 );
-		carritoCompra.appendItemCarito(new ItemCarrito(prodTest, 2));
+		carritoCompra.appendItemCarrito(new ItemCarrito(prodTest, 2));
 		prodTest = new Producto("Leche", 85.5 );
-		carritoCompra.appendItemCarito(new ItemCarrito(prodTest, 6));
+		carritoCompra.appendItemCarrito(new ItemCarrito(prodTest, 6));
 
 		Double rtdoEsperado = 2400.0;
 
 		
-		assertEquals(rtdoEsperado, carritoCompra.calcularTotalConDescuento());
+		try {
+			assertEquals(rtdoEsperado, carritoCompra.calcularTotalConDescuento());
+		} catch (DescuentoCarritoCeroException e) {
+			System.out.println(e.getMensaje());
+		}
 
 	}
 
 	@Test
-	public void test4_probarConDescuentoPorPorcentajeConTope() {
+	public void test5_probarConDescuentoPorPorcentajeConTope() {
 		Producto prodTest = new Producto("", 0.0);
 	
 		Descuento descuentoCarrito = new DescuentoPorcConTope("DESCUENTO PORCENTAJE CON TOPE", 10.0, 22.0);		
@@ -113,17 +133,22 @@ public class CarritoTest {
 		carritoCompra.setDesc(descuentoCarrito);
 		
 		prodTest = new Producto("Pan", 25.0 );
-		carritoCompra.appendItemCarito(new ItemCarrito(prodTest, 1));
+		carritoCompra.appendItemCarrito(new ItemCarrito(prodTest, 1));
 		prodTest = new Producto("Queso", 100.0 );
-		carritoCompra.appendItemCarito(new ItemCarrito(prodTest, 2));
+		carritoCompra.appendItemCarrito(new ItemCarrito(prodTest, 2));
 		prodTest = new Producto("Leche", 85.5 );
-		carritoCompra.appendItemCarito(new ItemCarrito(prodTest, 6));
+		carritoCompra.appendItemCarrito(new ItemCarrito(prodTest, 6));
 
 		Double rtdoEsperado = 716.0;
 		
-		assertEquals(rtdoEsperado, carritoCompra.calcularTotalConDescuento());
+		try {
+			assertEquals(rtdoEsperado, carritoCompra.calcularTotalConDescuento());
+		} catch (DescuentoCarritoCeroException e) {
+			System.out.println(e.getMensaje());
+		}
 
 	}
+	*/
 
-*/	
+
 }
